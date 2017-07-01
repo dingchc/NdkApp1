6a7cfe0caac518668855f62c814ea0cb02a4ed93
dingchongci <dingchongci@aspirecn.com>
2017-07-01 21:21:37 +0800

    add v5 in test1

--- app/src/main/java/com/dcc/ndkapp1/MainActivity.java
+++ app/src/main/java/com/dcc/ndkapp1/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int test1V5 = 0;
 
+    private int test1V6 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
